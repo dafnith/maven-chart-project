5b2b01186c7fca1628e0e9ece26897e1686a822f
dafnith <dafnitheodorou1999@gmail.com>
2020-04-23 01:19:28 +0300

    Add test cases for method IsPrime
    - testIsPrime() normal test cases
    - testIsPrimeShouldThrowExceptionOnLowerThanTwoInput() test case for the exception caused when
    the input value is < 2.
    Testing is performed with a @Rule

--- seip2020_practical_assignments/unittesting/src/test/java/math/MyMathTest.java
+++ seip2020_practical_assignments/unittesting/src/test/java/math/MyMathTest.java
@@ -1,6 +1,9 @@
 package math;
 
+import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class MyMathTest {
 	
@@ -10,5 +13,28 @@ public class MyMathTest {
 	public void testFactorialException() {
 		mm.factorial(-1);
 		mm.factorial(13);
+	}
+	
+	@Test
+	public void testIsPrime() {
+		Assert.assertEquals(true, mm.isPrime(2));
+		Assert.assertEquals(true, mm.isPrime(3));
+		Assert.assertEquals(false, mm.isPrime(6));
+		Assert.assertEquals(true, mm.isPrime(97));
+	}
+	
+	
+	/*
+	 * A test case for the exception caused when
+	 * the input value is < 2. Testing
+	 * the exception is performed with a @Rule
+	 */
+	@Rule 
+	public ExpectedException thrown = ExpectedException.none(); //initialize it to .none()
+	@Test 
+	public void testIsPrimeShouldThrowExceptionOnLowerThanTwoInput() {
+		thrown.expect(IllegalArgumentException.class);
+		thrown.expectMessage("number must be >=2");
+		mm.isPrime(1);
 	}
 }
