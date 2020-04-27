e63032c67546bfa8bec656273bfdbf4e7570ef4e
dafnith <dafnitheodorou1999@gmail.com>
2020-04-27 22:54:26 +0300

    Mock MyMath behaviour

--- seip2020_practical_assignments/unittesting/src/test/java/math/ArrayOperationsTest.java
+++ seip2020_practical_assignments/unittesting/src/test/java/math/ArrayOperationsTest.java
@@ -13,11 +13,18 @@ public class ArrayOperationsTest {
 	@Test
 	public void testFindPrimesInFile() {
 		FileIO fileio = mock(FileIO.class);
-		MyMath mm = new MyMath();
-		//MyMath mm = new MyMath();
+		MyMath mm = mock(MyMath.class);
+		
+
 		
 		String path = "src/test/resources/mixed.txt";
 		when(fileio.readFile(path)).thenReturn(new int[] {2,6,3,6,5,29});
+
+		when(mm.isPrime(2)).thenReturn(true);
+		when(mm.isPrime(6)).thenReturn(false);
+		when(mm.isPrime(3)).thenReturn(true);
+		when(mm.isPrime(5)).thenReturn(true);
+		when(mm.isPrime(29)).thenReturn(true);
 		
 		ArrayOperations arrop = new ArrayOperations();
 		int[] expected = {2,3,5,29};
